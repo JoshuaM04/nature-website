a20e4c62dc94aaceae4aeb1ef44303914591a433
Viper <pruncle21@gmail.com>
2026-08-30 19:36:31 -0500

    Added a array of objects that displays the available routes, a state variable for storing an active rate, and a map callback function for rendering the routes.

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,7 +1,29 @@
 'use client';
 import { DialogTrigger, ModalOverlay, Modal, Dialog, Heading, Button} from 'react-aria-components';
+import { useState } from 'react';
 
 export default function Navigation() {
+    let routes = [
+        {
+            number: '01',
+            route: 'Index',
+        },
+        {
+            number: '02',
+            route: 'Gallery',
+        },
+        {
+            number: '03',
+            route: 'Field Notes',
+        },
+        {
+            number: '04',
+            route: 'About',
+        }
+    ];
+
+    const [activeRoute, setActiveRoute] = useState('Index');
+
     return (
         <div className="masthead-rule flex justify-between items-center gap-5 p-5 w-full">
             <div>
@@ -17,7 +39,7 @@ export default function Navigation() {
                     </svg>
                 </Button>
 
-                <ModalOverlay className="fixed inset-0 z-50 bg-surface">
+                <ModalOverlay className="fixed inset-0 z-50 bg-page">
                     <Modal className="w-full h-full">
                         <Dialog>
                             <div className="masthead-rule flex justify-between items-center gap-5 p-5 bg-page">
@@ -31,6 +53,25 @@ export default function Navigation() {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                     </svg>
                                 </Button>
+                            </div>
+
+                            <div>
+                                {
+                                    routes.map((item, index) => (
+                                        <div 
+                                            onClick={() => setActiveRoute(item.route)}
+                                            className={`${item.route === activeRoute ? 'bg-surface' : 'bg-page'} filter-bar-rule flex justify-between items-center p-5 border-b`} key={index}>
+                                            <div className="flex gap-5">
+                                                <div>{item.number}</div>
+                                                <div>{item.route}</div>
+                                            </div>
+
+                                            <div className={`${item.route === activeRoute ? 'masthead-tagline block' : 'hidden'}`}>
+                                                Current
+                                            </div>
+                                        </div>
+                                    ))
+                                }
                             </div>
                         </Dialog>
                     </Modal>
